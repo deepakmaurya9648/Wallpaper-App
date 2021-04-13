8fe5b8f8d72ad8fc5124cfefb687a70ea9a12fc6
DEEPAK MAURYA <dkmaurya9648@gmail.com>
2021-04-13 10:08:36 +0530

    added the feature of category seach of wallaper

--- wallpaperweb/lib/views/image_views.dart
+++ wallpaperweb/lib/views/image_views.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class ImageView extends StatefulWidget {
+  @override
+  _ImageViewState createState() => _ImageViewState();
+}
+
+class _ImageViewState extends State<ImageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
